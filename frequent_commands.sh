2f50517aa3d2a34c77cc2daa45d65432ab57d4db
Lee Raulin <leeraulin@gmail.com>
2018-04-28 21:55:25 -0400

    Set Dvorak, swap caps and escape in X11

--- frequent_commands.sh
+++ frequent_commands.sh
@@ -3,7 +3,8 @@
 autoload -U zsh-newuser-install
 zsh-newuser-install -f
 
-# Swap caps and escape
-# User: ~/.xinitrc
-# Global: /etc/X11/xinit/xinitrc
+# Swap caps and escape for current X11 session
 setxkbmap -option caps:escape
+
+# Set X11 keymap Dvorak, swapped escape/caps
+sudo localectl set-x11-keymap us pc105 dvorak caps:swapescapes
